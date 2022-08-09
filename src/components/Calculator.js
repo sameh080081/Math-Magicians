901a8f200d9eee424e7ad4d32b5b65294933e055
sameh <sameh080081@gmail.com>
2022-08-10 00:26:36 +0400

    fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,44 @@
-import React from "react";
-import './Calculator.css'
+import React from 'react';
+import './Calculator.css';
 
 class Calculator extends React.Component {
-  render(){
+  render() {
     return (
-    <React.Fragment>
-    <div className="calc">
-    <p className="screen">0</p>
-    <div>
-    <button>AC</button>
-    <button>+/-</button>
-    <button>%</button>
-    <button className="orange">+</button>
-    </div>
-    <div>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button className="orange">x</button>
-    </div>
-    <div>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button className="orange">-</button>
-    </div>
-    <div>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button className="orange">+</button>
-    </div>
-    <div>
-    <button className="zero">0</button>
-    <button>.</button>
-    <button className="orange">=</button>
-    </div>
-    </div>
-    </React.Fragment>);
+      <React.Fragment>
+      <div className="calc">
+      <p className="screen">0</p>
+      <div>
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="orange">+</button>
+      </div>
+      <div>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="orange">x</button>
+      </div>
+      <div>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="orange">-</button>
+      </div>
+      <div>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="orange">+</button>
+      </div>
+      <div>
+      <button type="button" className="zero">0</button>
+      <button type="button">.</button>
+      <button type="button" className="orange">=</button>
+      </div>
+      </div>
+      </React.Fragment>
+    );
   }
 }
 
