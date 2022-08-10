c07db3879db780873fe163598d0d9a476b2d734d
sameh <sameh080081@gmail.com>
2022-08-10 20:31:38 +0400

    add events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,63 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: 0,
+    }
+    this.operationClick = this.operationClick.bind(this);
+    this.screen = this.screen.bind(this);
+  }
+
+  operationClick = (e) => {
+    const operation = e.target.value;
+    const result = calculate(this.state, operation);
+    this.setState(result);
+  }
+
+  screen = (e) => {
+    this.setState({
+      total: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="calc">
-        <p className="screen">0</p>
+        <p className="screen"onChange={this.screen}>{this.state.next || this.state.total || 0}</p>
         <div>
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="orange">+</button>
+          <button type="button" onClick={this.operationClick}  value='AC'>AC</button>
+          <button type="button" onClick={this.operationClick}  value='+/-'>+/-</button>
+          <button type="button" onClick={this.operationClick}  value='%'>%</button>
+          <button type="button" onClick={this.operationClick}  value='÷' className="orange">÷</button>
         </div>
         <div>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="orange">x</button>
+          <button type="button" onClick={this.operationClick} value="7">7</button>
+          <button type="button" onClick={this.operationClick} value="8">8</button>
+          <button type="button" onClick={this.operationClick} value="9">9</button>
+          <button type="button" onClick={this.operationClick}  value='x' className="orange">x</button>
         </div>
         <div>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="orange">-</button>
+          <button type="button" onClick={this.operationClick} value="4">4</button>
+          <button type="button" onClick={this.operationClick} value="5">5</button>
+          <button type="button" onClick={this.operationClick} value="6">6</button>
+          <button type="button" onClick={this.operationClick}  value='-' className="orange">-</button>
         </div>
         <div>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="orange">+</button>
+          <button type="button" onClick={this.operationClick} value="1">1</button>
+          <button type="button" onClick={this.operationClick} value="2">2</button>
+          <button type="button" onClick={this.operationClick} value="3">3</button>
+          <button type="button" onClick={this.operationClick}  value='+' className="orange">+</button>
         </div>
         <div>
-          <button type="button" className="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="orange">=</button>
+          <button type="button" className="zero" onClick={this.operationClick} value="0">0</button>
+          <button type="button" onClick={this.operationClick}  value='.'>.</button>
+          <button type="button" onClick={this.operationClick}  value='=' className="orange">=</button>
         </div>
       </div>
     );
