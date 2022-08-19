61de26870f5e425655f2d18cdaeaf148ce581510
sameh <sameh080081@gmail.com>
2022-08-19 17:58:07 +0400

    update package.json add title to calc

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -29,37 +29,39 @@ class Calculator extends React.Component {
   render() {
     const { total, next } = this.state;
     return (
-      <div className="calc">
+      <div className="container">
         <h2>Let&apos;s do some math!</h2>
-        <p className="screen" onChange={this.screen}>{ next || total || 0 }</p>
-        <div>
-          <button type="button" onClick={this.operationClick} value="AC">AC</button>
-          <button type="button" onClick={this.operationClick} value="+/-">+/-</button>
-          <button type="button" onClick={this.operationClick} value="%">%</button>
-          <button type="button" onClick={this.operationClick} value="÷" className="orange">÷</button>
-        </div>
-        <div>
-          <button type="button" onClick={this.operationClick} value="7">7</button>
-          <button type="button" onClick={this.operationClick} value="8">8</button>
-          <button type="button" onClick={this.operationClick} value="9">9</button>
-          <button type="button" onClick={this.operationClick} value="x" className="orange">x</button>
-        </div>
-        <div>
-          <button type="button" onClick={this.operationClick} value="4">4</button>
-          <button type="button" onClick={this.operationClick} value="5">5</button>
-          <button type="button" onClick={this.operationClick} value="6">6</button>
-          <button type="button" onClick={this.operationClick} value="-" className="orange">-</button>
-        </div>
-        <div>
-          <button type="button" onClick={this.operationClick} value="1">1</button>
-          <button type="button" onClick={this.operationClick} value="2">2</button>
-          <button type="button" onClick={this.operationClick} value="3">3</button>
-          <button type="button" onClick={this.operationClick} value="+" className="orange">+</button>
-        </div>
-        <div>
-          <button type="button" className="zero" onClick={this.operationClick} value="0">0</button>
-          <button type="button" onClick={this.operationClick} value=".">.</button>
-          <button type="button" onClick={this.operationClick} value="=" className="orange">=</button>
+        <div className="calc">
+          <p className="screen" onChange={this.screen}>{ next || total || 0 }</p>
+          <div>
+            <button type="button" onClick={this.operationClick} value="AC">AC</button>
+            <button type="button" onClick={this.operationClick} value="+/-">+/-</button>
+            <button type="button" onClick={this.operationClick} value="%">%</button>
+            <button type="button" onClick={this.operationClick} value="÷" className="orange">÷</button>
+          </div>
+          <div>
+            <button type="button" onClick={this.operationClick} value="7">7</button>
+            <button type="button" onClick={this.operationClick} value="8">8</button>
+            <button type="button" onClick={this.operationClick} value="9">9</button>
+            <button type="button" onClick={this.operationClick} value="x" className="orange">x</button>
+          </div>
+          <div>
+            <button type="button" onClick={this.operationClick} value="4">4</button>
+            <button type="button" onClick={this.operationClick} value="5">5</button>
+            <button type="button" onClick={this.operationClick} value="6">6</button>
+            <button type="button" onClick={this.operationClick} value="-" className="orange">-</button>
+          </div>
+          <div>
+            <button type="button" onClick={this.operationClick} value="1">1</button>
+            <button type="button" onClick={this.operationClick} value="2">2</button>
+            <button type="button" onClick={this.operationClick} value="3">3</button>
+            <button type="button" onClick={this.operationClick} value="+" className="orange">+</button>
+          </div>
+          <div>
+            <button type="button" className="zero" onClick={this.operationClick} value="0">0</button>
+            <button type="button" onClick={this.operationClick} value=".">.</button>
+            <button type="button" onClick={this.operationClick} value="=" className="orange">=</button>
+          </div>
         </div>
       </div>
     );
